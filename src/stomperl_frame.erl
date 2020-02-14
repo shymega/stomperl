ddfa22a715cc0edd5bc9b3a6cc42ac33a8f943f1
Dom Rodriguez <shymega@shymega.org.uk>
2020-02-14 16:40:38 +0000

    Minor commenting adjustments

--- src/stomperl_frame.erl
+++ src/stomperl_frame.erl
@@ -8,7 +8,7 @@
 %% `[collate_opts/1,collate_opts/2]` based
 %% on `concatenate_options` function in
 %% from: https://github.com/igb/Erlang-STOMP-Client
-%% Cheers, Ian (igb).
+%% Kudos to igb.
 -spec collate_opts(list()) -> list().
 collate_opts([H | T]) ->
   {Key, Value} = H,
@@ -22,9 +22,9 @@ collate_opts([]) -> [].
 
 -spec new_frame(string(), list()) -> list().
 new_frame(CMD, Opts) ->
-	lager:debug("Creating a STOMP frame on request, cmd: ~p", [CMD]),
+    lager:debug("Creating a STOMP frame on request, cmd: ~p", [CMD]),
 
-	% `F` is a record, an internal representation of the frame.  Final
+    % `F` is a record, an internal representation of the frame.  Final
     % processing of the frame will turn it into a binary term with
     % correct delimiter characters, etc.
     F = #frame{
